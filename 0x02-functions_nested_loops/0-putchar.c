18b2876360d965e3863843ff30b2044038e96549
rufida24 <rufidamohamed6@gmail.com>
2023-09-12 18:56:18 -0700

    -putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,18 @@
 
 /**
  * main - Entrt point
- * Description: prints -putcher using putchar prototype
+ * Description: prints -putchar using putchar prototype
  *
  * Return: Always 0 (Success)
 */
 int main(void)
-	char str{} = "-putcher";
+{
+	char str[] = "-putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
-		-putcher(str[ch]);
-	-putcher('\n');
+		-putchar(str[ch]);
+	-putchar('\n');
 
 	return (0);
+}
