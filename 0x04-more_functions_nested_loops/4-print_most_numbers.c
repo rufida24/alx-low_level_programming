7fcd45a3e51f91d121a00853fd29ea5ef12b89bb
rufida24 <rufidamohamed6@gmail.com>
2023-09-14 18:48:54 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers -prints 12356789
+ * print_most_numbers -prints 01356789
  * Return:void
  */
 
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 char c;
 
 	for (c = '0'; c <= '9'; c++)
-	for (!(c == '2' || c == '4'))
+	if (!(c == '2' || c == '4'))
 	_putchar(c);
 	}
 	_putchar('\n');
